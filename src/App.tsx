bdfe423025ce654903afc79fd0f3d05967980b57
Eran Leasure <eran.leasure@gmail.com>
2022-09-05 09:20:31 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <img src={reactLogo} alt='React.js logo' />
+        <h1>React Todos</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
